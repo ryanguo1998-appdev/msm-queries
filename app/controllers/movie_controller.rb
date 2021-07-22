ce6731e50e6ff10c622b2841102006e663e2f045
ryanguo1998 <rguo98@uchicago.edu>
2021-07-22 03:05:37 +0000

    Directors and Movies done

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,5 +1,13 @@
 class MovieController < ApplicationController
   def index
+    @all_movies = Movie.all
+
     render({ :template => "mov_templates/index.html.erb"})
+  end
+
+  def details
+    @spec_movie = Movie.where({ :id => params[:mov_id]}).at(0)
+
+    render({ :template => "mov_templates/details.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get("/", { :controller => "application", :action => "homepage" })
+  
   get("/directors", { :controller => "director", :action => "index"})
   get("/directors/:dir_id", { :controller => "director", :action => "details"})
+  
+  get("/movies", { :controller => "movie", :action => "index"})
+  get("/movies/:mov_id", { :controller => "movie", :action => "details"})
 end
